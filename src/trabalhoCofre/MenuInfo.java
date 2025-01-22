c35800da7648b67ccf55f7270d575f985c1b2abc
Laís Santana <lais.desantana05@gmail.com>
2025-01-21 23:58:08 -0300

    Update MenuInfo.java

--- src/trabalhoCofre/MenuInfo.java
+++ src/trabalhoCofre/MenuInfo.java
@@ -6,6 +6,7 @@ public class MenuInfo implements Menu{
 	 
 		@Override
 	public void infoEntrada() {
+		
         System.out.println("\n--- COFRINHO ---\n");
         System.out.println("SELECIONE UMA OPÇÃO: ");
         System.out.println("1 - Adicionar moeda");
@@ -14,21 +15,22 @@ public class MenuInfo implements Menu{
         System.out.println("4 - Calcular total convertido para Real");
         System.out.println("5. - Sair");
 	}
-
+	
 		@Override
 	public void adicionarMoeda() {
-		System.out.println("\nSelecione a moeda para adicionar:");
+	
+	System.out.println("\nSelecione a moeda para adicionar:");
         System.out.println("1 - Dólar");
         System.out.println("2 - Euro");
-        System.out.println("3 - Real");
-			
-		}
-		
+        System.out.println("3 - Real");		
+	}
+	
 		@Override
-		public void removerMoeda() {
-			System.out.println("\nSelecione a moeda para adicionar:");
-	        System.out.println("1 - Dólar");
-	        System.out.println("2 - Euro");
-	        System.out.println("3 - Real");
-		}
+	public void removerMoeda() {
+			
+	System.out.println("\nSelecione a moeda para adicionar:");
+	System.out.println("1 - Dólar");
+	System.out.println("2 - Euro");
+	System.out.println("3 - Real");
+	}
 }
